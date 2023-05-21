646cfb30b01c25c6c429991f2086f8e348e3417a
bvizank <bvizank@ncsu.edu>
2023-05-21 13:16:52 -0400

    small changes

--- utils.py
+++ utils.py
@@ -2,15 +2,174 @@ import pandas as pd
 import numpy as np
 import numba as nb
 import os
+import wntr
+import copy
 
 
-__all__ = ['read_data', 'choose']
+__all__ = ['setup', 'read_data', 'choose']
 
 
-def init_wntr():
+def setup(network):
+    # Create a water network model
+    if network == "micropolis":
+        inp_file = 'Input Files/MICROPOLIS_v1_inc_rest_consumers.inp'
+        data = pd.read_excel(r'Input Files/Micropolis_pop_at_node.xlsx')
+    elif network == "mesopolis":
+        inp_file = 'Input Files/Mesopolis.inp'
+        data = pd.read_excel(r'Input Files/Mesopolis_pop_at_node.xlsx')
+    base_demands, pattern_list, wn = init_wntr(inp_file)
+
+    # input the number of agents required at each node type at each time
+    node_id = data['Node'].tolist()
+    maxpop_node = data['Max Population'].tolist()
+    if network == "mesopolis":
+        house_num = data['HOUSE'].tolist()
+        # create dictionary with the number of houses per node
+        house_num = dict(zip(node_id, house_num))
+    else:
+        house_num = None
+
+    # Creating dictionary with max pop at each terminal node
+    node_capacity = dict(zip(node_id, maxpop_node))
+
+    node_dict = dict()
+    if network == "micropolis":
+        # Node kinds are:(Pattern number - Kind of node)
+        # 1: Commercial – Cafe
+        # 2: Residential
+        # 3: Industrial, factory with 3 shifts
+        # 4: Commercial – Dairy Queen
+        # 5: Commercial – Curches, schools, city hall, post office
+        # Only terminal nodes count. So only nodes with prefix 'TN'
+
+        # Cafe nodes (only 1)
+        node_dict['cafe'] = find_nodes(1, pattern_list, network)
+        # residential nodes
+        node_dict['res'] = find_nodes(2, pattern_list, network)
+        # Industrial nodes
+        node_dict['ind'] = find_nodes(3, pattern_list, network)
+        # Nodes dairy queen
+        node_dict['dq'] = find_nodes(4, pattern_list, network)
+        # Rest of commercial nodes like schools, churches etc.
+        node_dict['com'] = find_nodes(5, pattern_list, network)
+    elif network == "mesopolis":
+        # pattern types: air, com, res, ind, nav
+        node_dict['air'] = find_nodes('air', pattern_list, network)
+        node_dict['com'] = find_nodes('com', pattern_list, network)
+        node_dict['res'] = find_nodes('res', pattern_list, network)
+        node_dict['ind'] = find_nodes('ind', pattern_list, network)
+        node_dict['nav'] = find_nodes('nav', pattern_list, network)
+        node_dict['cafe'] = find_nodes('cafe', pattern_list, network)
+
+    terminal_nodes = list()
+    for key in node_dict:
+        terminal_nodes += node_dict[key]
+
+    # finish setup process by loading distributions of agents at each node type
+    # and media data.
+    pop_dict = load_distributions(network)
+    media, bbn_params, wfh_patterns = load_media()
+
+    return (node_dict, node_capacity, house_num, pop_dict, media, bbn_params,
+            wfh_patterns, terminal_nodes, wn)
+
+
+def init_wntr(inp_file):
+    # initialize the water network model with wntr
+    wn = wntr.network.WaterNetworkModel(inp_file)
+
+    # List all nodes and all nodes with demand
+    lst_nodes = wn.node_name_list
+    dem_nodes = [node for node in lst_nodes if hasattr(wn.get_node(node), 'demand_timeseries_list')]
+    # creating list of base demands
+    lst_base_demands = dict()
+    node_patterns = dict()
+    for node in lst_nodes:
+        junction = wn.get_node(node)
+        if node in dem_nodes:
+            time_list = junction.demand_timeseries_list[0]
+            # get base demand value for each node with a demand
+            lst_base_demands[node] = copy.deepcopy(time_list.base_value)
+
+            # Find demand patterns for each node
+            node_patterns[node] = copy.deepcopy(time_list.pattern_name)
+
+    return (lst_base_demands, node_patterns, wn)
+
+
+def find_nodes(type, pattern_list, network):
+    output = list()
+    for k, v in pattern_list.items():
+        if v != 'DefPat' and network == "micropolis":
+            # print(f"v: {v}; type: {type}; are they equal {int(v) == int(type)}")
+            if int(v) == int(type) and k[0:2] == 'TN':
+                output.append(k)
+            else:
+                continue
+        elif network == "mesopolis":
+            if v == type and k[0:2] == 'TN':
+                output.append(k)
+
+    return output
+
+
+def load_distributions(network):
     '''
-    Initialize the wntr model and get the nodes
-    '
+    Load in the data on how many agents are supposed to be at each node type
+    at each time of the day.
+
+    THIS IS DEPENDENT ON THE NETWORK
+    '''
+    # Import table for timesteps per nodetypes per hour
+    if network == "micropolis":
+        node_pop = pd.read_excel(r'Input Files/Micropolis_pop_at_node_kind.xlsx')
+    elif network == "mesopolis":
+        node_pop = pd.read_excel(r'Input Files/Mesopolis_pop_at_node_kind.xlsx')
+
+    pop_dict = dict()
+
+    pop_dict['ind'] = node_pop['indust.'].to_numpy()
+    pop_dict['res'] = node_pop['resident'].to_numpy()
+    pop_dict['com'] = node_pop['comm.'].to_numpy()
+    pop_dict['cafe'] = node_pop['rest.'].to_numpy()
+    if network == "mesopolis":
+        pop_dict['nav'] = node_pop['navy'].to_numpy()
+        pop_dict['air'] = node_pop['air'].to_numpy()
+    pop_dict['sum'] = node_pop['sum'].to_numpy()
+
+    return (pop_dict)
+
+
+def load_media():
+    '''
+    Load in the necessary media data including sleep, TV, and radio data
+    Also load the BBN parameters from Dryhurst et al. 2020 and the Lakewood
+    COVID-19 residential patterns.
+
+    THIS DATA IS NOT DEPENDENT ON NETWORK
+    '''
+
+    # Load TV, RADIO, SLEEP data
+    sleep_data = pd.read_excel(r'Input Files/sleep_data.xlsx')
+    sleep_distr = sleep_data['sleep_data'].to_numpy()
+
+    radio_data = pd.read_excel(r'Input Files/Radio_data.xlsx')
+    radio_distr = radio_data['radio_data'].to_numpy()
+
+    tv_data = pd.read_excel(r'Input Files/TV_data.xlsx')
+    tv_distr = tv_data['tv_data'].to_numpy()
+
+    media = {'sleep': sleep_distr,
+             'radio': radio_distr,
+             'tv': tv_distr}
+
+    # Load agent parameters for BBN predictions
+    bbn_params = pd.read_csv(r'Input Files/all_bbn_data.csv')
+
+    # Load in the new residential patterns from Lakewood data
+    wfh_patterns = pd.read_csv(r'Input Files/res_patterns/normalized_res_patterns.csv')
+
+    return (media, bbn_params, wfh_patterns)
 
 
 def read_data(loc, read_list):
