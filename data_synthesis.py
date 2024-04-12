adc29773e625e0565c34193f51b1f57a2f82fd8c
Brent Vizanko <bvizank@ncsu.edu>
2024-04-12 11:34:02 -0400

    new ppe run

--- data_synthesis.py
+++ data_synthesis.py
@@ -12,7 +12,7 @@ rank = comm.Get_rank()
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='logs/log'+str(rank), filemode='w', level=logging.INFO)
 
-data_dir = 'Output Files/30_groc_equity/'
+data_dir = 'Output Files/30_ppe_equity/'
 # data_dir = '/Users/vizan/Library/CloudStorage/OneDrive-NorthCarolinaStateUniversity/Research/Equity/excess_data/30_base_equity'
 # data_dir = 'D:/OneDrive - North Carolina State University/Research/Code/ABM/Excess Data/wfh_30/'
 
