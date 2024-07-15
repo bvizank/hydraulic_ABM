f15ca7f26667ea896d98a637b9f6d9d155fc9e46
Brent Vizanko <bvizank@ncsu.edu>
2024-07-15 09:46:34 -0400

    new figures

--- graphics.py
+++ graphics.py
@@ -2,16 +2,16 @@ from base import Graphics
 
 
 days = 180
-plots = Graphics(publication=False, error='se', days=days)
+plots = Graphics(publication=True, error='se', days=days)
 
 ''' Demand plots '''
-# plots.demand_plots()
+plots.demand_plots()
 
 ''' Flow plots '''
 # plots.flow_plots()
 
 ''' Age plots '''
-# plots.age_plots()
+plots.age_plots()
 
 ''' Industrial distance plots '''
 # plots.ind_dist_plots()
@@ -26,16 +26,16 @@ plots = Graphics(publication=False, error='se', days=days)
 # plots.make_equity_plots()
 
 ''' Cost plots '''
-# plots.make_cost_plots()
+plots.make_cost_plots()
 
 ''' Tap water avoidance plots '''
-# plots.make_twa_plots()
+plots.make_twa_plots()
 
 ''' SEIR plot '''
-# plots.make_seir_plot(100)
+plots.make_seir_plot(180)
 
 ''' Make single plots '''
-plots.make_single_plots('2024-07-02_11-15_0_results', 150)
+# plots.make_single_plots('2024-07-02_11-15_0_results', 150)
 
 # ''' Export comparison stats '''
 # print("WFH model stats: " + str(plots.calc_model_stats(wn, only_wfh['avg_seir_data'], only_wfh['avg_age']/3600)))
