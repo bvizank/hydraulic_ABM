112e7d5893fe13b9590a91d739d11e964949cc4e
bvizank <bvizank@ncsu.edu>
2025-02-16 18:52:46 -0500

    testing osm

--- parcel_test.py
+++ parcel_test.py
@@ -5,4 +5,10 @@ wn = wntr.network.WaterNetworkModel('Input Files/cities/clinton/clinton.inp')
 city = 'clinton'
 dir = 'Input Files/cities/clinton/'
 
-node_buildings = ci.make_building_list(wn, city, dir)
+# node_buildings = ci.make_building_list(wn, city, dir)
+
+service_area = ci.get_water_utility_service_areas('clinton')
+print(service_area)
+osm_buildings = ci.get_osm_buildings_within_area(service_area)
+
+print(osm_buildings)
